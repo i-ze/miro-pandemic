8245b5c7da6e2acc2ef9e7a80054582975678ad1
ze <ze@iodrift.com>
2020-11-29 04:27:32 +0100

    APPID config
    Signed-off-by: ze <ze@iodrift.com>

--- plugin-config.js
+++ plugin-config.js
@@ -1,1 +1,3 @@
-const APP_ID = '3074457351840249299'; //THIS IS THE PANDEMIC PLUGIN (APP) ID. Needed to store metadata.
+const APP_ID_DEV = '3074457351840249299'; //THIS IS THE PANDEMIC PLUGIN (APP) ID. Needed to store metadata.
+const APP_ID_PROD = '3074457352082479119'; //THIS IS THE PANDEMIC PLUGIN (APP) ID. Needed to store metadata.
+const APP_ID = document.location.host === "document.location.host" ? APP_ID_PROD : APP_ID_DEV; //THIS IS THE PANDEMIC PLUGIN (APP) ID. Needed to store metadata.
